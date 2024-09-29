40d15f95bec70b68ddcb94ead8681e4d2b9c46b0
shahzor khan <shahzoorkhan321@gmail.com>
2024-09-29 19:32:28 +0500

    generating audio for the story using elevenlab is done

--- lib/elevenlab.py
+++ lib/elevenlab.py
@@ -1,2 +1,43 @@
-#elevenlabs integrations resource
-# https://github.com/elevenlabs/elevenlabs-python
+from openaiapi import story_generator
+from elevenlabs import ElevenLabs, VoiceSettings
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+api_key = os.getenv("ELEVENLABS_KEY")
+
+
+def audio_generator(story):
+
+    client = ElevenLabs(
+        api_key=api_key,
+    )
+    
+    response = client.text_to_speech.convert(
+        voice_id="jsCqWAovK2LkecY7zXl4",
+        optimize_streaming_latency="0",
+        output_format="mp3_22050_32",
+        text=story,
+        voice_settings=VoiceSettings(
+            stability=0.1,
+            similarity_boost=0.3,
+            style=0.2,
+        ),
+    )
+
+    # The response is a generator, so we need to iterate through it to get the audio data
+    audio_data = b"".join(response)  # Concatenating all chunks into a single byte string
+    
+  
+    with open("output_audio.mp3", "wb") as audio_file:
+        audio_file.write(audio_data)
+
+    print("Audio file generated successfully!")
+    return "output_audio.mp3"
+
+
+
+story = story_generator("car", "thrill", "horror")
+
+audio_generator(story)
