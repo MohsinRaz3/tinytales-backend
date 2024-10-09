c8346eeb14df54532c569bd5d556e918298061e3
MohsinRaz3 <mohsinraz3@gmail.com>
2024-10-10 03:49:49 +0500

    main response

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Body
+from fastapi.responses import JSONResponse
 from lib.openaiapi import story_generator
 from lib.pydmodels import GetStoryData, StoryResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -35,7 +36,8 @@ async def generate_story(story_data: GetStoryData = Body(...))-> dict:
             story_data.genre, 
             story_data.characters
         )
-        return {"result": generated_story}
+        data  = {"result": generated_story}
+        return JSONResponse(content=data)
 
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
